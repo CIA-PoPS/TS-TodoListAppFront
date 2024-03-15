d1e98c976095c2edd29d8efeba462fd83253ad78
devdl11 <dl11developer@gmail.com>
2024-03-15 09:10:03 +0100

    Update Tailwind CSS colors and remove unused logo.svg file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "columbia-blue": "#BFD7EA",
+        "cadet-gray": "#91AEC1",
+        "air-force-blue": "#508CA4",
+        "sea-green": "#0A8754",
+        "cal-poly-green": "#004F2D",
+      },
+    },
   },
   plugins: [],
 };
