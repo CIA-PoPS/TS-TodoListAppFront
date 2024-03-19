c6274159148c0d67a302e536a30d96e8a32cb1e5
devdl11 <dl11developer@gmail.com>
2024-03-19 16:58:44 +0100

    [add] SimpleTodo creation (step 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import TodosContainer from './components/TodosContainer';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TodosContainer from "./components/TodosContainer";
+import { TDElementTypes } from "./components/TodoElement";
+import { SimpleTodoFactory, TodoTypes } from "./shared/todos";
 
 function App() {
+  const [todos, setTodos] = useState<TDElementTypes.ParamsType[]>([]);
+  const [isAddingTD, setAddingTD] = useState(false);
+
+  const saveTodo = (newTodo: TodoTypes.TodoDTO) => {
+    setTodos([...todos.slice(1), { todo: SimpleTodoFactory(newTodo) }]);
+    setAddingTD(false);
+  };
+
+  const createTodo = () => {
+    setTodos([{ callback: saveTodo }, ...todos]);
+    setAddingTD(true);
+  };
+
   return (
     <div className="App">
-      <div className='mycontainer'>
-          <Header></Header>
-          <TodosContainer todos={[]}></TodosContainer>
+      <div className="mycontainer">
+        <Header addTodoTrigger={createTodo} enabled={!isAddingTD} />
+        <TodosContainer todos={todos}></TodosContainer>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,28 @@
-import PlusCircleIcon from '@heroicons/react/24/outline/PlusCircleIcon';
-import './Header.css';
-import '@heroicons/react/24/outline/PlusCircleIcon'
+import PlusCircleIcon from "@heroicons/react/24/outline/PlusCircleIcon";
+import "./Header.css";
+import "@heroicons/react/24/outline/PlusCircleIcon";
+import { VoidTrigger } from "../shared/utility";
 
-function Header() {
-    return (
-        <div className="Header">
-            <div id='AppTitle'>
-                Todo
-                <div className='italic inline'>
-                    CIA
-                </div>
-            </div>
-            <div id='ButtonsBar'>
-                <PlusCircleIcon id='AddTodo'></PlusCircleIcon>
-            </div>
-        </div>
-    )
-}
+type HeaderProps = {
+  addTodoTrigger: VoidTrigger;
+  enabled: boolean;
+};
+
+export const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <div className="Header">
+      <div id="AppTitle">
+        Todo
+        <div className="italic inline">CIA</div>
+      </div>
+      <div id="ButtonsBar">
+        <PlusCircleIcon
+          className={`AddTodo ${props.enabled ? "AddTodo-Act" : "AddTodo-Dea"}`}
+          onClick={() => (props.enabled ? props.addTodoTrigger() : 0)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
